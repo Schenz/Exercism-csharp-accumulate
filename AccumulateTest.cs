1296af9e7b71c865024ab1408b6c3074a4606177
Schenz <bschenz@gmail.com>
2018-08-21 09:59:56 -0400

    final solution

--- AccumulateTest.cs
+++ AccumulateTest.cs
@@ -6,28 +6,16 @@ using Xunit;
 public class AccumulateTest
 {
     [Fact]
-    public void Empty_accumulation_produces_empty_accumulation()
-    {
-        Assert.Equal(new int[0], new int[0].Accumulate(x => x * x));
-    }
+    public void Empty_accumulation_produces_empty_accumulation() => Assert.Equal(new int[0], new int[0].Accumulate(x => x * x));
 
-    [Fact(Skip = "Remove to run test")]
-    public void Accumulate_squares()
-    {
-        Assert.Equal(new[] { 1, 4, 9 }, new[] { 1, 2, 3 }.Accumulate(x => x * x));
-    }
+    [Fact]
+    public void Accumulate_squares() => Assert.Equal(new[] { 1, 4, 9 }, new[] { 1, 2, 3 }.Accumulate(x => x * x));
 
-    [Fact(Skip = "Remove to run test")]
-    public void Accumulate_upcases()
-    {
-        Assert.Equal(new List<string> { "HELLO", "WORLD" }, new List<string> { "hello", "world" }.Accumulate(x => x.ToUpper()));
-    }
+    [Fact]
+    public void Accumulate_upcases() => Assert.Equal(new List<string> { "HELLO", "WORLD" }, new List<string> { "hello", "world" }.Accumulate(x => x.ToUpper()));
 
-    [Fact(Skip = "Remove to run test")]
-    public void Accumulate_reversed_strings()
-    {
-        Assert.Equal("eht kciuq nworb xof cte".Split(' '), "the quick brown fox etc".Split(' ').Accumulate(Reverse));
-    }
+    [Fact]
+    public void Accumulate_reversed_strings() => Assert.Equal("eht kciuq nworb xof cte".Split(' '), "the quick brown fox etc".Split(' ').Accumulate(Reverse));
 
     private static string Reverse(string value)
     {
@@ -36,15 +24,10 @@ public class AccumulateTest
         return new string(array);
     }
 
-    [Fact(Skip = "Remove to run test")]
-    public void Accumulate_within_accumulate()
-    {
-        var actual = new[] { "a", "b", "c" }.Accumulate(c =>
-            string.Join(" ", new[] { "1", "2", "3" }.Accumulate(d => c + d)));
-        Assert.Equal(new[] { "a1 a2 a3", "b1 b2 b3", "c1 c2 c3" }, actual);
-    }
+    [Fact]
+    public void Accumulate_within_accumulate() => Assert.Equal(new[] { "a1 a2 a3", "b1 b2 b3", "c1 c2 c3" }, new[] { "a", "b", "c" }.Accumulate(c => string.Join(" ", new[] { "1", "2", "3" }.Accumulate(d => c + d))));
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Accumulate_is_lazy()
     {
         var counter = 0;
@@ -55,9 +38,6 @@ public class AccumulateTest
         Assert.Equal(3, counter);
     }
 
-    [Fact(Skip = "Remove to run test")]
-    public void Accumulate_allows_different_return_type()
-    {
-        Assert.Equal(new[] { "1", "2", "3" }, new[] { 1, 2, 3 }.Accumulate(x => x.ToString()));
-    }
+    [Fact]
+    public void Accumulate_allows_different_return_type() => Assert.Equal(new[] { "1", "2", "3" }, new[] { 1, 2, 3 }.Accumulate(x => x.ToString()));
 }
